4ef97cd40594fdb57623e5ca5bd2972765354bd1
eru <epenkman@thoughtworks.com>
2017-03-12 19:34:58 +1000

    create default account

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ randomUtf8=$(head -c 64 /dev/urandom | base64)
 (umask 377 ; echo $randomUtf8 > .password)
 
 geth --datadir .ethereum-test init config/test/genesis.json
-geth --mine --password .password js node_modules/embark/js/mine.js
+geth --password .password account new
+geth --mine js node_modules/embark/js/mine.js
