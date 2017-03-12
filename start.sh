dc13f4f4469fc4544e7092787413f99ec57a2938
eru <epenkman@thoughtworks.com>
2017-03-12 19:30:28 +1000

    update start script ot create account

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+randomUtf8=$(head -c 64 /dev/urandom | base64)
+(umask 377 ; echo $randomUtf8 > .password)
+
 geth --datadir .ethereum-test init config/test/genesis.json
-geth --mine js node_modules/embark/js/mine.js
+geth --mine --password .password js node_modules/embark/js/mine.js
